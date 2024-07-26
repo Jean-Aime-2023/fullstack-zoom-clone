a94d18b4abf6008464a513f42c275ec4cf4119c4
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2024-07-26 09:20:40 +0200

    created clerk user manually - stream provider

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -4,39 +4,50 @@ import Image from 'next/image';
 import React, { useState } from 'react';
 import HomeCard from './HomeCard';
 import { useRouter } from 'next/router';
+import MeetingModal from './MeetingModal';
 
 const MeetingTypeList = () => {
+  // const router = useRouter()
   const [meetingState,setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>()
+  const createMeeting =()=>{
+
+  }
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
-      <HomeCard 
-       img="/icons/add-meeting.svg"
-       title="New Meeting"
-       description="Start an instant meeting"
-       handleClick={()=>setMeetingState('isJoiningMeeting')}
-       className="bg-orange-1"
+      <HomeCard
+        img="/icons/add-meeting.svg"
+        title="New Meeting"
+        description="Start an instant meeting"
+        handleClick={() => setMeetingState('isInstantMeeting')}
       />
-      <HomeCard 
-       img="/icons/schedule.svg"
-       title="Schedule Meeting"
-       description="Plan your meeting"
-       handleClick={()=>setMeetingState('isScheduleMeeting')}
-       className="bg-blue-1"
+      <HomeCard
+        img="/icons/join-meeting.svg"
+        title="Join Meeting"
+        description="via invitation link"
+        className="bg-blue-1"
+        handleClick={() => setMeetingState('isJoiningMeeting')}
       />
-      <HomeCard 
-       img="/icons/recordings.svg"
-       title="View Recordings"
-       description="Check out your recordings"
-       handleClick={()=>setMeetingState('isJoiningMeeting')}
-       className="bg-purple-1"
+      <HomeCard
+        img="/icons/schedule.svg"
+        title="Schedule Meeting"
+        description="Plan your meeting"
+        className="bg-purple-1"
+        handleClick={() => setMeetingState('isScheduleMeeting')}
       />
-      <HomeCard 
-       img="/icons/join-meeting.svg"
-       title="Join Meeting"
-       description="Via invitation link"
-       handleClick={()=>setMeetingState('isJoiningMeeting')}
-       className="bg-yellow-1"
+      <HomeCard
+        img="/icons/recordings.svg"
+        title="View Recordings"
+        description="Meeting Recordings"
+        className="bg-yellow-1"
+        handleClick={() => router.push('/recordings')}
       />
+
+      <MeetingModal
+        isOpen={meetingState === 'isInstantMeeting'}
+        onClose={() => setMeetingState(undefined)}
+        className="text-center"
+        buttonText="Start Meeting"
+        handleClick={createMeeting} title={'Start an instant meeting'}      />
     </section>
   );
 };
---
+++ providers/StreamClientProvider.tsx
@@ -0,0 +1,20 @@
+import {
+    StreamVideoClient,
+    StreamVideo,
+  } from '@stream-io/video-react-sdk';
+import { ReactNode, useEffect, useState } from 'react';
+  
+  const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+  
+  const StreamVideoProvider = ({children}:{children:ReactNode}) => {
+    const [videoClient,setVideoClient] = useState<StreamVideoClient>()
+    useEffect(()=>{
+        
+    },[]);
+    return (
+      <StreamVideo client={videoClient}>
+
+      </StreamVideo>
+    );
+  };
+  export default StreamVideoProvider;
