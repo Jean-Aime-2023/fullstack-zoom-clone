980ceabcc70fb26feb931ea3373bed1a30240f93
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2024-07-29 10:46:19 +0200

    final changes

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -15,7 +15,7 @@ const Home = () => {
   return (
     <section className="flex  flex-col gap-5 text-white">
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
-        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
+        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11 max-lg:p-9">
           <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
             Upcoming Meeting at: 12:30 PM
           </h2>
